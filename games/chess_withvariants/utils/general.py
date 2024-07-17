9821bdb50a87f753769b604d016fac3a00e52bb7
freezmcgeez <118069402+freezmcgeez@users.noreply.github.com>
2024-07-17 18:25:24 +0200

    fairy stockfish for mac OS

--- games/chess_withvariants/utils/general.py
+++ games/chess_withvariants/utils/general.py
@@ -15,7 +15,7 @@ def get_path_stockfish_bin():
     if platform == "linux" or platform == "linux2":
         return os.path.join(get_path_file(), "resources","fairy-stockfish_x86-64" )
     elif platform  == 'darwin':
-        return os.path.join(get_path_file(), "resources","fairy-stockfish_x86-64" )
+        return os.path.join(get_path_file(), "resources","fairy-stockfish_macOS" )
 
 
 
